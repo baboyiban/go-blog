22d0d22a9ba9a0afdd6d41f02ee184c44ea2e588
baboyiban <chl11wq12@gmail.com>
2025-03-25 15:08:38 +0900

    dotenv 테스트

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,18 +1,10 @@
 import { defineConfig } from "astro/config";
 import { config } from "dotenv";
 
-// Docker 환경 대응
-try {
-  config({ path: "/app/.env" }); // ← 절대 경로 지정
-} catch (e) {
-  console.log("Local .env loaded");
-}
+config();
 
 export default defineConfig({
   build: {
-    assetsPrefix:
-      process.env.NODE_ENV === "production"
-        ? `https://cdn.${process.env.DOMAIN}`
-        : undefined,
+    assetsPrefix: `https://cdn.${process.env.DOMAIN ?? "localhost"}`,
   },
 });
