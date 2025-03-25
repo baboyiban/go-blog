0d218c26b57daa7cd876c8848dd903c93e7146df
baboyiban <chl11wq12@gmail.com>
2025-03-25 14:45:51 +0900

    cdn 설정

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import { config } from "dotenv";
-
-config();
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    assetsPrefix: `https://cdn.${process.env.DOMAIN || "localhost"}`,
+    inlineStylesheets: "auto",
+  },
+});
