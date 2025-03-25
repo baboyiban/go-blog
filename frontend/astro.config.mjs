aef70ff83f2549c9f0f6bb6a84ba076cdeeed711
baboyiban <chl11wq12@gmail.com>
2025-03-25 15:41:05 +0900

    Update astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   build: {
     assetsPrefix: process.env.DOMAIN
-      ? `https://cdn.${process.env.DOMAIN}`
+      ? `https://cdn.choidaruhan.xyz`
       : "/_astro", // ← 개발 환경 폴백
   },
 });
