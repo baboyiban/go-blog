a8ed784b363797f8473ad034797f476935784463
baboyiban <chl11wq12@gmail.com>
2025-03-26 17:08:30 +0900

    backend main.go, database.go 수정

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/baboyiban/go-blog/internal/database"
 	"github.com/baboyiban/go-blog/internal/routes"
@@ -10,9 +11,17 @@ import (
 )
 
 func main() {
-	db := database.ConnectDB()
-	defer db.Close()
+    db := database.ConnectDB()
+    defer db.Close()
 
-	router := routes.SetupRouter(db) // Now matches the signature
-	log.Fatal(http.ListenAndServe(":8080", router))
+    router := routes.SetupRouter(db)
+
+    // 환경변수에서 포트 가져오기
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080" // 기본값
+    }
+
+    log.Printf("Starting server on port %s", port)
+    log.Fatal(http.ListenAndServe(":"+port, router))
 }
--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -2,15 +2,43 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func ConnectDB() *sql.DB {
-	dsn := "gouser:gopassword@tcp(mysql:3306)/goblog?parseTime=true&tls=true"
+	// 환경 변수에서 데이터베이스 연결 정보 가져오기
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	// 기본값 설정 (환경 변수가 비어 있을 경우)
+	if dbHost == "" {
+		dbHost = "mysql:3306" // 기본값은 Docker 네트워크의 서비스 이름
+	}
+
+	// DSN 구성
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",
+		dbUser, dbPassword, dbHost, dbName)
+
+	// TLS 설정이 필요한 경우 추가 (옵션)
+	if os.Getenv("DB_USE_TLS") == "true" {
+		dsn += "&tls=true"
+	}
+
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err.Error())
 	}
+
+	// 연결 테스트
+	err = db.Ping()
+	if err != nil {
+		panic(fmt.Sprintf("데이터베이스 연결 실패: %v", err))
+	}
+
 	return db
 }
