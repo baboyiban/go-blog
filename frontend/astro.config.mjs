ba713b0605336a811994219a5c8ab5e1c4bb2de9
baboyiban <chl11wq12@gmail.com>
2025-03-26 15:43:57 +0900

    frontend dockerfile 테스트

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,10 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export default defineConfig({
-  build: {
-    assetsPrefix: process.env.DOMAIN
-      ? `https://${process.env.DOMAIN}`
-      : "/_astro", // ← 개발 환경 폴백
-  },
-});
+export default defineConfig({});
