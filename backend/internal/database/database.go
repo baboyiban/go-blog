e8532ac614f613e66b73ccf298a9ea58cc5ff0a6
baboyiban <chl11wq12@gmail.com>
2025-03-27 11:14:33 +0900

    DB_ -> MYSQL_

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -13,17 +13,17 @@ import (
 
 func ConnectDB() *sql.DB {
 	// 환경 변수에서 데이터베이스 연결 정보 가져오기
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbDATABASE := os.Getenv("DB_DATABASE")
+	dbUser := os.Getenv("MYSQL_USER")
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
+	dbHost := os.Getenv("MYSQL_HOST")
+	dbDATABASE := os.Getenv("MYSQL_DATABASE")
 
-	// DSN 구성
+	// DSN 구성MYSQL
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",
 		dbUser, dbPassword, dbHost, dbDATABASE)
 
 	// TLS 설정이 필요한 경우 추가 (옵션)
-	if os.Getenv("DB_USE_TLS") == "true" {
+	if os.Getenv("MYSQL_USE_TLS") == "true" {
 		dsn += "&tls=true"
 	}
 
